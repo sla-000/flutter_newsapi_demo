6d8e86b9ee9ab58c5d5ffb50ff5fd50ab0930856
Slava Ryabinin <sla.000@ya.ru>
2023-07-23 10:15:31 +0300

    feat: Add news api class

--- lib/src/di/di.dart
+++ lib/src/di/di.dart
@@ -1,14 +1,17 @@
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 import 'package:injectable/injectable.dart';
+import 'package:news_api/news_api.dart';
 
 import 'di.config.dart';
 
-final getIt = GetIt.instance;
+final di = GetIt.instance;
 
 @InjectableInit()
 void configureDependencies() {
-  getIt.init();
+  di.init();
 
-  getIt.registerLazySingleton<http.Client>(http.Client.new);
+  di.registerLazySingleton<http.Client>(http.Client.new);
+
+  di.registerLazySingleton<NewsApi>(() => NewsApi(client: di<http.Client>()));
 }
---
+++ packages/news_api/lib/news_api.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
+
+import 'models/sources/sources_request.dart';
+import 'models/sources/sources_response.dart';
+
+const _url = 'newsapi.org';
+const _scheme = 'https';
+
+class NewsApi {
+  NewsApi({
+    required this.client,
+  });
+
+  @protected
+  final http.Client client;
+
+  Future<SourcesResponse> getSources({
+    required SourcesRequest sourcesRequest,
+  }) async {
+    final url = Uri(
+      scheme: _scheme,
+      host: _url,
+      path: 'v2/top-headlines/sources',
+      queryParameters: sourcesRequest.toJson(),
+    );
+
+    final response = await client.get(url);
+
+    final json = _checkResponse(response);
+
+    return SourcesResponse.fromJson(json);
+  }
+
+  Map<String, dynamic> _checkResponse(http.Response response) {
+    final statusCode = response.statusCode;
+
+    if (statusCode < 200 || statusCode >= 300) {
+      throw HttpException(
+          'Wrong status code=$statusCode, reason=${response.reasonPhrase}');
+    }
+
+    final json = jsonDecode(response.body);
+
+    if (json is! Map<String, dynamic>) {
+      throw HttpException('Wrong format of body=$json');
+    }
+
+    if (json['status'] != 'ok') {
+      throw HttpException(
+          'Request error code=${json['code']}, message=${json['message']}');
+    }
+
+    return json;
+  }
+}
