308e843bbf8c87ea99704978672b1dbd38ab2111
Slava Ryabinin <sla.000@ya.ru>
2023-07-24 16:09:12 +0300

    feat: Add favorite button

--- lib/src/presentation/top_headlines/widgets/articles_list.dart
+++ lib/src/presentation/top_headlines/widgets/articles_list.dart
@@ -2,12 +2,14 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 import '../../../di/di.dart';
 import '../../../domain/models/article_domain_model.dart';
 import '../cubits/top_headlines_cubit.dart';
 
 const _switchDuration = Duration(milliseconds: 500);
+const _spacing = 12.0;
 
 class ArticlesList extends StatelessWidget {
   const ArticlesList({
@@ -86,24 +88,61 @@ class OneArticle extends StatelessWidget {
   final ArticleDomainModel article;
 
   @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Wrap(
-              children: [
-                Text(article.publishedAt),
-                if (article.author != null) Text(article.author!),
+  Widget build(BuildContext context) => Card(
+        child: Padding(
+          padding: const EdgeInsets.all(_spacing),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Wrap(
+                      spacing: 16,
+                      children: [
+                        Text(
+                          _getTime(context, article.publishedAt),
+                          style: Theme.of(context).textTheme.titleSmall,
+                        ),
+                        if (article.author != null)
+                          Text(
+                            article.author!,
+                            style: Theme.of(context).textTheme.titleSmall,
+                          ),
+                      ],
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.favorite_outline),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              const SizedBox(height: _spacing),
+              Text(
+                article.title,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(height: _spacing),
+              Text(article.description),
+              if (article.content != null) ...[
+                const SizedBox(height: _spacing),
+                Text(article.content!)
               ],
-            ),
-            Text(article.title),
-            Text(article.description),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
+
+  String _getTime(BuildContext context, String time) {
+    final localTime = DateTime.parse(time).toLocal();
+    final locale = Localizations.localeOf(context).toString();
+
+    final dateStr = DateFormat.yMMMMEEEEd(locale).format(localTime);
+    final timeStr = DateFormat.Hms(locale).format(localTime);
+
+    return '$dateStr $timeStr';
   }
 }
